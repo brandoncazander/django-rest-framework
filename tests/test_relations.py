dd1500f92da437e1384d89944e8bd52d65f30c88
Brandon Cazander <brandon.cazander@multapplied.net>
2015-01-28 17:08:34 -0800

    Add failing test for bug in HyperlinkedRelatedField and NamespacedVersioning.

--- tests/test_relations.py
+++ tests/test_relations.py
@@ -1,9 +1,27 @@
 from .utils import mock_reverse, fail_reverse, BadType, MockObject, MockQueryset
-from django.core.exceptions import ImproperlyConfigured
+from django.conf.urls import patterns, url, include
+from django.core.exceptions import ImproperlyConfigured, ObjectDoesNotExist
 from rest_framework import serializers
-from rest_framework.test import APISimpleTestCase
+from rest_framework import versioning
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.test import APISimpleTestCase, APIRequestFactory
 import pytest
 
+factory = APIRequestFactory()
+request = factory.get('/')  # Just to ensure we have a request in the serializer context
+
+dummy_view = lambda request, pk: None
+
+included_patterns = [
+    url(r'^example/(?P<pk>\d+)/$', dummy_view, name='example-detail')
+]
+
+urlpatterns = patterns(
+    '',
+    url(r'^v1/', include(included_patterns, namespace='v1')),
+    url(r'^example/(?P<pk>\d+)/$', dummy_view, name='example-detail')
+)
 
 class TestStringRelatedField(APISimpleTestCase):
     def setUp(self):
@@ -45,6 +63,34 @@ class TestPrimaryKeyRelatedField(APISimpleTestCase):
         representation = self.field.to_representation(self.instance)
         assert representation == self.instance.pk
 
+
+class TestHyperlinkedRelatedField(APISimpleTestCase):
+    urls = 'tests.test_relations'
+
+    def setUp(self):
+        class HyperlinkedMockQueryset(MockQueryset):
+            def get(self, **lookup):
+                for item in self.items:
+                    if item.pk == int(lookup.get('pk', -1)):
+                        return item
+                raise ObjectDoesNotExist()
+
+        self.queryset = HyperlinkedMockQueryset([
+            MockObject(pk=1, name='foo'),
+            MockObject(pk=2, name='bar'),
+            MockObject(pk=3, name='baz')
+        ])
+        self.field = serializers.HyperlinkedRelatedField(
+            view_name='example-detail',
+            queryset=self.queryset
+        )
+        self.field.reverse = mock_reverse
+        self.field._context = {'request': request}
+
+    def test_matching_url(self):
+        print self.field.to_internal_value('/example/3/')
+        print self.field.to_internal_value('/v1/example/3/')
+        assert False
 
 class TestHyperlinkedIdentityField(APISimpleTestCase):
     def setUp(self):
